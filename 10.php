f1f38d1962de4ca456eebe66688c1e18370d4594
Bro_010 <118188740+tregamir@users.noreply.github.com>
2022-12-22 15:51:39 +0300

    Create 10.php

---
+++ 10.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+<?php
+$file = file("doc.txt");
+preg_match("/(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])/",$file[0],$ff);
+print_r( $ff[0]);
+?>
+</body>
+</html>
